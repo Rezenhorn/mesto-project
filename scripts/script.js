ab25eefa0b9f28784fcfd1f3921fddafd2cd481a
Dmitry Fomichev <torman37rus@gmail.com>
2023-05-11 15:56:40 +0300

    feat: add popup % js scripts

---
+++ scripts/script.js
@@ -0,0 +1,32 @@
+let popup = document.querySelector('.popup');
+let editButton = document.querySelector('.profile__edit-button');
+let closeButton = document.querySelector('.popup__close-button');
+let formElement = document.querySelector('.popup__edit-form');
+let nameInput = document.querySelector('#input-name');
+let bioInput = document.querySelector('#input-bio');
+let profileName = document.querySelector('.profile__name');
+let profileBio = document.querySelector('.profile__bio');
+
+
+function popupOpened() {
+  popup.classList.add('popup_opened');
+  nameInput.value = profileName.textContent;
+  bioInput.value = profileBio.textContent;
+}
+
+function popupClosed() {
+  popup.classList.remove('popup_opened');
+}
+
+function handleFormSubmit (evt) {
+  evt.preventDefault();
+  newName = nameInput.value;
+  newBio = bioInput.value;
+  profileName.textContent = newName;
+  profileBio.textContent = newBio;
+  popupClosed();
+}
+
+formElement.addEventListener('submit', handleFormSubmit);
+editButton.addEventListener('click', popupOpened);
+closeButton.addEventListener('click', popupClosed);
