d97e9a7f7cdd626d50353c4560e545be08e526e0
Dmitry Fomichev <torman37rus@gmail.com>
2023-05-25 10:02:35 +0300

    fix: remove initial text in add form

--- scripts/index.js
+++ scripts/index.js
@@ -6,20 +6,20 @@ const addCardButton = document.querySelector('.profile__add-button');
 // элементы editProfilePopup:
 const editProfilePopup = document.querySelector('.popup__form_edit');
 const closeEditProfilePopupButton = editProfilePopup.querySelector('.popup__close-button');
-const editProfileForm = editProfilePopup.querySelector('.form')
+const editProfileForm = editProfilePopup.querySelector('.form');
 const profileNameInput = document.querySelector('.form__form-item_profile_name');
 const profileBioInput = document.querySelector('.form__form-item_profile_bio');
 // элементы addCardPopup:
 const addCardPopup = document.querySelector('.popup__form_add');
 const closeAddCardPopupButton = addCardPopup.querySelector('.popup__close-button');
 const addCardForm = addCardPopup.querySelector('.form');
-const cardPlaceInput = document.querySelector('.form__form-item_place_name')
-const cardLinkInput = document.querySelector('.form__form-item_place_link')
+const cardPlaceInput = document.querySelector('.form__form-item_place_name');
+const cardLinkInput = document.querySelector('.form__form-item_place_link');
 // элементы imagePopup:
 const imagePopup = document.querySelector('.popup__image');
 const closeImagePopupButton = imagePopup.querySelector('.popup__close-button');
-// другие элементы
-const cardGrid = document.querySelector('.cards-grid')
+// другие элементы:
+const cardGrid = document.querySelector('.cards-grid');
 const cardTemplate = document.querySelector('#card').content;
 
 function createCard(name, link) {
@@ -29,12 +29,12 @@ function createCard(name, link) {
   cardElement.querySelector('.card__heading').textContent = name;
   addLikeCardListener(cardElement.querySelector('.card__like-button'));
   addDeleteCardListener(cardElement.querySelector('.card__delete-button'));
-  addImageCardListener(cardElement.querySelector('.card__image'))
+  addImageCardListener(cardElement.querySelector('.card__image'));
   return cardElement;
 }
 
 function addCard(name, link) {
-  const card = createCard(name, link)
+  const card = createCard(name, link);
   cardGrid.prepend(card);
 }
 
@@ -50,35 +50,37 @@ function popupClosed(popup) {
   popup.classList.remove('popup_opened');
 }
 
-function handleEditFormSubmit (evt) {
+function handleEditFormSubmit(evt) {
   evt.preventDefault();
   profileName.textContent = profileNameInput.value;
   profileBio.textContent = profileBioInput.value;
   popupClosed(editProfilePopup);
 }
 
-function handleAddFormSubmit (evt) {
+function handleAddFormSubmit(evt) {
   evt.preventDefault();
+  cardPlaceInput.value = "";
+  cardLinkInput.value = "";
   addCard(cardPlaceInput.value, cardLinkInput.value);
   popupClosed(addCardPopup);
 }
 
-function addLikeCardListener (likeButton) {
+function addLikeCardListener(likeButton) {
   likeButton.addEventListener(
     'click', () => likeButton.classList.toggle('card__like-button_active')
   );
-};
+}
 
-function addDeleteCardListener (deleteButton) {
+function addDeleteCardListener(deleteButton) {
   deleteButton.addEventListener(
     'click', () => {
       const closestCard = deleteButton.closest('.card');
       closestCard.remove();
     }
   );
-};
+}
 
-function addImageCardListener (image) {
+function addImageCardListener(image) {
   image.addEventListener(
     'click', () => {
       imagePopup.querySelector('.figure__image').src = image.src;
@@ -86,7 +88,7 @@ function addImageCardListener (image) {
       imagePopup.classList.add('popup_opened');
     }
   );
-};
+}
 
 addCardButton.addEventListener('click', () => addCardPopup.classList.add('popup_opened'));
 editProfileButton.addEventListener('click', editProfilePopupOpened);
